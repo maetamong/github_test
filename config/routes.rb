40c081fb718e4d59498b0cafe73b88924b208fc8
maetamong <daserism@gmail.com>
2016-08-12 11:31:54 +0900

    Create timeline page 3

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
-  get 'wall/write'
-
-  get 'wall/posts'
-
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
+
+Timeline::Application.routes.draw do
+  get 'wall/write' => 'wall#write'
+  post 'wall/posts' => 'wall#posts'
 end
