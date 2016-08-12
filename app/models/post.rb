96e05fd89f113d8e76203367b029acb8bae87ca7
maetamong <daserism@gmail.com>
2016-08-12 14:32:07 +0900

    migration models two tables

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,4 @@
 class Post < ApplicationRecord
+	has_many	:comments
 	validates :content, presence: {message: "Please write something."}
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160812023255) do
+ActiveRecord::Schema.define(version: 20160812052401) do
+
+  create_table "comments", force: :cascade do |t|
+    t.integer  "post_id"
+    t.string   "name"
+    t.string   "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "posts", force: :cascade do |t|
     t.string   "name"
