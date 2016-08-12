fc7391cdfb93a1d0af3002d70b722f0a27b88b2d
maetamong <daserism@gmail.com>
2016-08-13 01:44:28 +0900

    comments add

--- config/routes.rb
+++ config/routes.rb
@@ -1,19 +1,12 @@
 Gourmet::Application.routes.draw do
   root 'foods#posts'
-
   get "/:category" => 'foods#posts_category' 
-
   get 'foods/show/:id'=> 'foods#show'
-
   get 'foods/write'
-
   post 'foods/write_complete'
-
   get 'foods/edit/:id' => 'foods#edit'
-
   post 'foods/edit_complete'
-
   get 'foods/delete_complete/:id' => 'foods#delete_complete'
-
+	post 'foods/write_comment_complete'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
