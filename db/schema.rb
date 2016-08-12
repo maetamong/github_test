1c7e39bfe78eb8da426aa2ce037e02fa9e1f2c17
maetamong <daserism@gmail.com>
2016-08-12 15:04:48 +0900

    migrataion models User table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160812052401) do
+ActiveRecord::Schema.define(version: 20160812060028) do
 
   create_table "comments", force: :cascade do |t|
     t.integer  "post_id"
@@ -23,6 +23,13 @@ ActiveRecord::Schema.define(version: 20160812052401) do
   create_table "posts", force: :cascade do |t|
     t.string   "name"
     t.string   "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "username"
+    t.string   "password"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
