5ec9453872f24edc2035842ba77e05a3ba2f1c6e
maetamong <daserism@gmail.com>
2016-08-16 00:13:57 +0900

    logincheck logout

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,11 @@
 class ApplicationController < ActionController::Base
  # protect_from_forgery with: :exception
+	def login_check
+		@current_user = nil
+		if !session[:user_id].nil?
+			@current_user = User.find(session[:user_id])
+		else
+			redirect_to "/users/login"
+		end
+	end
 end
